b612c46c3ee456f62419797b72e43104d142f7f7
Gabriel Schinca <gabriel.schinca94@gmail.com>
2022-01-13 15:42:35 -0300

    added readme, also changed counter default initial state to 0

--- useCounter.js
+++ useCounter.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-export const useCounter = ( initialState = 10 ) => {
+export const useCounter = ( initialState = 0 ) => {
     
     const [counter, setCounter] = useState(initialState);
 
